8bd48aebeb13a866469ad6594ee27bc49ab27dd0
MD. TAHAMID HASAN <112802620+tahamidcse@users.noreply.github.com>
2026-09-16 09:21:22 -0700

    Create edmond.cpp

---
+++ edmond.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 1e9;
+
+int n;
+vector<vector<int>> capacity;
+vector<vector<int>> adj;
+
+int bfs(int s, int t, vector<int>& parent) {
+    fill(parent.begin(), parent.end(), -1);
+    parent[s] = -2;
+    queue<pair<int, int>> q;
+    q.push({s, INF});
+
+    while (!q.empty()) {
+        int cur = q.front().first;
+        int flow = q.front().second;
+        q.pop();
+
+        for (int next : adj[cur]) {
+            if (parent[next] == -1 && capacity[cur][next]) {
+                parent[next] = cur;
+                int new_flow = min(flow, capacity[cur][next]);
+                if (next == t)
+                    return new_flow;
+                q.push({next, new_flow});
+            }
+        }
+    }
+
+    return 0;
+}
+
+int maxflow(int s, int t) {
+    int flow = 0;
+    vector<int> parent(n);
+    int new_flow;
+
+    while ((new_flow = bfs(s, t, parent))) {
+        flow += new_flow;
+        int cur = t;
+        while (cur != s) {
+            int prev = parent[cur];
+            capacity[prev][cur] -= new_flow;
+            capacity[cur][prev] += new_flow;
+            cur = prev;
+        }
+    }
+
+    return flow;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m;
+    cin >> n >> m;
+
+    capacity.assign(n, vector<int>(n, 0));
+    adj.assign(n, vector<int>());
+
+    for (int i = 0; i < m; i++) {
+        int u, v, cap;
+        cin >> u >> v >> cap;
+        // If multiple edges between u and v, add capacities
+        if (capacity[u][v] == 0 && capacity[v][u] == 0) {
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        capacity[u][v] += cap;
+    }
+
+    int s, t;
+    cin >> s >> t;
+
+    cout << "Max flow from " << s << " to " << t << " = " 
+         << maxflow(s, t) << "\n";
+
+    return 0;
+}
